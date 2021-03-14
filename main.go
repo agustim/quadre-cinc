49f9075afd93766b15f4945d66dd86be269b41bb
Agustí Moll <agusti@gmail.com>
2021-03-14 12:49:57 +0100

    some changes

--- main.go
+++ main.go
@@ -1,38 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-const TrobarTotesLesSolucions = true
+const TrobarTotesLesSolucions = false
+const IterationLog = 1000000
 
 func main() {
 
-	var pendents, bons []Quadre
+	var pendents, bons, ciclics []Quadre
 	var solucionat, sortirBucle bool
+	var q, freq *Quadre
+	var startTime time.Time
+	var firstSoluctionTime time.Duration
+	var MaxValue int
+
+	startTime = time.Now()
 
 	solucionat = false
 	sortirBucle = false
-	q := Init()
-	q.First()
-
-	pendents = append(pendents, *q)
+	pendents = Firsts()
+	freq = Init()
+	MaxValue = 0
 
 	for i := 0; len(pendents) != 0 && !sortirBucle; i++ {
 		//agafem l'últim q així tindirem a avançar i tenir els grups de pendents més controlats
 		var lpendents = len(pendents) - 1
 		q = &pendents[lpendents]
 		if q.Final() {
+			if !solucionat {
+				firstSoluctionTime = time.Since(startTime)
+			}
 			bons = append(bons, *q)
 			solucionat = true
 			if !TrobarTotesLesSolucions {
 				sortirBucle = true
 			}
+			if q.Ciclic() {
+				ciclics = append(ciclics, *q)
+			}
+		}
+		if MaxValue < q.lastNum {
+			MaxValue = q.lastNum
 		}
 		pendents = pendents[:lpendents]
 		pendents = append(pendents, q.NextStep()...)
-		fmt.Printf("MVActual: %d Pendents: %d - Iteracions: %d - Solucionat: %t\n", q.lastNum, len(pendents), i, solucionat)
+
+		if i%IterationLog == 0 {
+			fmt.Printf("MaxValue: %d Pendents: %d Iteracions: %dM Bons: %d Ciclics: %d Temps: %s\n",
+				MaxValue, len(pendents), i/1000000, len(bons), len(ciclics), time.Since(startTime))
+		}
 	}
-	for _, q := range bons {
+
+	if TrobarTotesLesSolucions {
+		for _, q := range bons {
+			hFreqBons, wFreqBons := q.WhereIsFirst()
+			freq.cela[hFreqBons][wFreqBons]++
+		}
+		fmt.Println("Freqüencis de Bons")
+		freq.Draw()
+		freq = Init()
+		for _, q := range ciclics {
+			hFreqCiclics, wFreqCiclics := q.WhereIsFirst()
+			freq.cela[hFreqCiclics][wFreqCiclics]++
+		}
+		fmt.Println("Freqüencia de ciclics")
+		freq.Draw()
+
+		fmt.Printf("Total Resultat bons: %d (Ciclics: %d), primera solució en %s, tot el process %s\n",
+			len(bons), len(ciclics), firstSoluctionTime, time.Since(startTime))
+	} else {
 		q.Draw()
+		fmt.Printf("Temps de calcul %s\n", time.Since(startTime))
 	}
-	fmt.Printf("Total Resultat bons: %d\n", len(bons))
+
 }
--- quadre.go
+++ quadre.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 )
 
-const Height = 5
-const Width = 5
+const Height = 9
+const Width = 9
 
 type Quadre struct {
 	lastNum int
@@ -56,8 +56,47 @@ func (q *Quadre) First() {
 	q.cela[q.lastH][q.lastW] = q.lastNum
 }
 
+func Firsts() []Quadre {
+	var qs []Quadre
+
+	for h := 0; h < Height; h++ {
+		for w := 0; w < Width; w++ {
+			q := Init()
+			q.lastH = h
+			q.lastW = w
+			q.lastNum = 1
+			q.cela[q.lastH][q.lastW] = q.lastNum
+			qs = append(qs, *q)
+		}
+	}
+	return qs
+}
+
 func (q *Quadre) Final() bool {
 	return q.lastNum == Height*Width
+}
+
+func (q *Quadre) Ciclic() bool {
+	return (q.lastH-3 >= 0 && q.cela[q.lastH-3][q.lastW] == 1) ||
+		(q.lastH+3 < Height && q.cela[q.lastH+3][q.lastW] == 1) ||
+		(q.lastW-3 >= 0 && q.cela[q.lastH][q.lastW-3] == 1) ||
+		(q.lastW+3 < Width && q.cela[q.lastH][q.lastW+3] == 1) ||
+		(q.lastH-2 >= 0 && q.lastW-2 >= 0 && q.cela[q.lastH-2][q.lastW-2] == 1) ||
+		(q.lastH-2 >= 0 && q.lastW+2 < Width && q.cela[q.lastH-2][q.lastW+2] == 1) ||
+		(q.lastH+2 < Height && q.lastW-2 >= 0 && q.cela[q.lastH+2][q.lastW-2] == 1) ||
+		(q.lastH+2 < Height && q.lastW+2 < Width && q.cela[q.lastH+2][q.lastW+2] == 1)
+}
+
+func (q *Quadre) WhereIsFirst() (int, int) {
+	var h, w int
+	for h = 0; h < Height; h++ {
+		for w = 0; w < Width; w++ {
+			if q.cela[h][w] == 1 {
+				return h, w
+			}
+		}
+	}
+	return h, w
 }
 
 func (q *Quadre) Moure(h int, w int) *Quadre {
